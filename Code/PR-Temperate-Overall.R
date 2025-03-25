dfcd64bcfbdd57b0adc8b9bfd47d4ab5811a7a35
shimogarakesh <shimogarakesh@gmail.com>
2025-03-25 15:54:30 +0100

    commit

--- Code/PR-Temperate-Overall.R
+++ Code/PR-Temperate-Overall.R
@@ -26,12 +26,13 @@ ggplot(df, aes(x = pc , y=phi)) +
                  outlier.colour="darkred", 
                  outlier.shape=21,
                  outlier.size=3, 
-                 outlier.alpha = 1, fill = "white")+ theme_bw() +
-    geom_point(inherit.aes = F, size = 2, stroke = 0.85, aes(x = pc, y = phi, color = species, shape = species), position = position_dodge(width=0.5)) 
-
-+
+                 outlier.alpha = 1, fill = "white")+ theme_minimal() +
+    geom_point(inherit.aes = F, size = 2, stroke = 0.85, aes(x = pc, y = phi, color = species, shape = species), position = position_dodge(width=0.5)) +
     scale_y_continuous(limits = c(0,1))+
-    scale_shape_manual(values = c(9,10,19,12,13,17,18))+
-    labs(x = "Temperate Tree Species", y = "", color = "Species", shape = "Species")+
-    theme(legend.text = element_text(family = "serif", face = "italic", size = 10) )
+    scale_shape_manual(values = c(9,10,19,12,13,17,18, 20,21,22,23,24,25, 8)) +
+    labs(x = "", y = "Phi", color = "Species", shape = "Species")+
+    theme(legend.text = element_text(family = "serif", face = "italic", size = 10) ) + coord_flip()
+
+
+
 
